ce8b30c6ece5eeafbbc35902405891a00b935395
opeyemiAkinwumi <opeyemiakinwumi14@gmail.com>
2024-10-23 00:42:37 +0100

    implemented the isp api

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,57 @@
 // import
+import { useState, useEffect } from "react";
 
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import Details from "./components/Details";
 
-function App() {
-  const position = [6.524379, 3.379206]
+// const API = "at_8ftFAGTO0jvqQarpl023HczXKf9Xk";
 
+function App() {
+  const [ipAddress, setIpAddress] = useState("");
+  const [isp, setIsp] = useState("");
+  const [location, setLocation] = useState("");
+  const [timezone, setTimezone] = useState("");
+
+  // States needed
+  // isp
+  // location
+  // timezone
+  // ipAddress
+
+  // useEffect(() => {
+  //   // Function to get the user's location
+  //   if (navigator.geolocation) {
+  //     navigator.geolocation.getCurrentPosition(
+  //       (position) => {
+  //         const { latitude, longitude } = position.coords;
+  //         setLocation({ latitude, longitude }); // Update state with coordinates
+  //       },
+  //       (error) => {
+  //         setError("Error retrieving location: " + error.message); // Handle error
+  //       }
+  //     );
+  //   } else {
+  //     setError("Geolocation is not supported by this browser.");
+  //   }
+  // }, []); // Empty dependency array ensures it runs once on component mount
+
+  const position = [39.831432958885664, -86.27218617386298];
+
+  useEffect(() => {
+    const url = `https://geo.ipify.org/api/v2/country?apiKey=at_8ftFAGTO0jvqQarpl023HczXKf9Xk&ipAddress=${ipAddress}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setIsp(data.isp);
+        setTimezone(data.location.timezone);
+        setLocation(`${data.location.region}, ${data.location.country}`);
+      });
+  }, [ipAddress]);
 
   return (
     <div className="font-main w-full h-screen relative ">
-      <div className="w-full h-[30%] bg-mobile lg:bg-desk bg-cover bg-center pt-10 space-y-5 ">
+      <div className="w-full h -[30%] bg-mobile lg:bg-desk bg-cover bg-center pt-10 space-y-5 ">
         <h1 className="text-center text-white text-lg lg:text-2xl font-semibold ">
           IP Address Tracker
         </h1>
@@ -22,6 +64,8 @@ function App() {
             type="text"
             placeholder="Search for any IP address or domain"
             className="w-full h-full px-5  "
+            value={ipAddress}
+            onChange={(e) => setIpAddress(e.target.value)}
           />
           <button className="w-20 h-full bg-black hover:bg-[#2b2b2b] flex items-center justify-center absolute right-0 top-0">
             <img src="icon-arrow.svg" alt="" />
@@ -31,12 +75,22 @@ function App() {
 
       {/* Details components */}
       <div className="absolute w-[80%] lg:w-[80%]  h-[400px] lg:h-[170px] left-0 right-0 top-[24%] mx-auto overflow-hidden rounded-xl shadow-sm shadow-gray-200 z-[999] ">
-        <Details />
+        <Details
+          isp={isp}
+          ipAddress={ipAddress}
+          timezone={timezone}
+          location={location}
+        />
       </div>
 
       {/* Map components */}
       <div id="map" className="w-full h-[70%]">
-        <MapContainer center={position} zoom={13} scrollWheelZoom={false} className="w-full h-full">
+        <MapContainer
+          center={position}
+          zoom={13}
+          scrollWheelZoom={false}
+          className="w-full h-full"
+        >
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,29 +1,29 @@
-function Details() {
+function Details({isp, location, ipAddress, timezone}) {
   return (
     <div className="w-full h-full bg-white lg:grid grid-cols-4 grid-rows-1 gap-5 pl-8 py-5 text-[#2B2B2B] ">
       <div className="w-full h-full text-center lg:text-left  lg:border-r pt-4 border-[#08080846] space-y-3">
         <p className="text-[#1615158e] text-xs font-bold tracking-wider ">
           IP ADDRESS
         </p>
-        <h3 className="font-bold text-2xl">192.212.174.101</h3>
+        <h3 className="font-bold text-2xl">{ipAddress}</h3>
       </div>
       <div className="w-full h-full text-center lg:text-left  lg:border-r pt-4 border-[#08080846] space-y-3">
         <p className="text-[#1615158e] text-xs font-bold tracking-wider ">
           LOCATION
         </p>
-        <h3 className="font-bold text-2xl max-w-[70%]">Victorian Island, lagos</h3>
+        <h3 className="font-bold text-2xl max-w-[70%]">{location}</h3>
       </div>
       <div className="w-full h-full text-center lg:text-left  lg:border-r pt-4 border-[#08080846] space-y-3">
         <p className="text-[#1615158e] text-xs font-bold tracking-wider ">
           TIMEZONE
         </p>
-        <h3 className="font-bold text-2xl">UTC-05:00</h3>
+        <h3 className="font-bold text-2xl">{timezone}</h3>
       </div>
       <div className="w-full h-full text-center lg:text-left   pt-4  space-y-3">
         <p className="text-[#1615158e] text-xs font-bold tracking-wider ">
           ISP
         </p>
-        <h3 className="font-bold text-2xl max-w-[50%]">SpaceX Starlink</h3>
+        <h3 className="font-bold text-2xl max-w-[50%]">{isp}</h3>
       </div>
     </div>
   );
